441c2727f0e3ad418b4710b545d50efadc702a99
Emmanuel Hernandez <carlos_e8246@hotmail.com>
2020-07-24 15:26:32 -0500

    added dropdown menu, fixed navbar not showing after clicking in an element. changed some titles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,16 +262,16 @@ function openNav() {
 	}
 }
 
-$('#myLinks>ul>li>a').on('click', function(){
-	var x = document.getElementById("myLinks");
-	setTimeout(()=>{
-		if (x.style.display === "block") {
-			x.style.display = "none";
-		} else {
-			x.style.display = "block";
-		}
-	},100);
-});
+// $('#myLinks>ul>li>a').on('click', function(){
+// 	var x = document.getElementById("myLinks");
+// 	setTimeout(()=>{
+// 		if (x.style.display === "block") {
+// 			x.style.display = "none";
+// 		} else {
+// 			x.style.display = "block";
+// 		}
+// 	},100);
+// });
 
 var limitFunc = function(){
     if (window.innerWidth>1080){
